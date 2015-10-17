66442bc1c4f2532d0815622905328e1886cae1a5
Sébastien <sebastien.biero@gmail.com>
2015-10-17 17:16:03 -0400

    fixed the app freezing on command line invocation handled the case where there is no media process

--- app/src/main/java/com/exlyo/camerarestarter/MainActivity.java
+++ app/src/main/java/com/exlyo/camerarestarter/MainActivity.java
@@ -39,28 +39,49 @@ public class MainActivity extends AppCompatActivity {
 
 	private void runRestartCameraShellCommand() throws Throwable {
 		final Process p = Runtime.getRuntime().exec("su");
-		final DataOutputStream os = new DataOutputStream(p.getOutputStream());
-		final BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		{
-			os.writeBytes("ps|grep media" + "\n");
+		DataOutputStream os = null;
+		BufferedReader br = null;
+		try {
+			os = new DataOutputStream(p.getOutputStream());
+			br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			os.writeBytes("line=$(ps|grep media.*[/]system[/]bin[/]mediaserver)" + "\n");
+			os.writeBytes("echo ${line}" + "\n");
 			String line = br.readLine();
-			while (line != null) {
-				if (line.startsWith("media") && line.endsWith("/system/bin/mediaserver")) {
-					line = line.replaceAll("media[ ]*", "");
-					final int pid = Integer.parseInt(line.substring(0, line.indexOf(" ")));
-					os.writeBytes("kill " + pid + "\n");
-					line = br.readLine();
-					if (line == null) {
-						throw new RuntimeException("FAIL");
-					}
-					return;
-				}
+			if (line.startsWith("media") && line.endsWith("/system/bin/mediaserver")) {
+				line = line.replaceAll("media[ ]*", "");
+				final int pid = Integer.parseInt(line.substring(0, line.indexOf(" ")));
+				os.writeBytes("kill " + pid + "\n");
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
 
-				line = br.readLine();
+		if (os != null) {
+			try {
+				os.writeBytes("exit\n");
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+			try {
+				os.flush();
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+			try {
+				os.close();
+			} catch (Throwable t) {
+				t.printStackTrace();
 			}
 		}
-		os.writeBytes("exit\n");
-		os.flush();
+
+		if (br != null) {
+			try {
+				br.close();
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+		}
+
 	}
 
 	private static void showToastMessage(@NonNull final Activity _activity, final String _messageText) {
